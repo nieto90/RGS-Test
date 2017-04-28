164db193b7973d9689a7ef2eea092593d1ca9d02
Javier Nieto <jnietom90@gmail.com>
2017-04-28 17:08:44 +0200

    Base models

--- rgs/matching/admin.py
+++ rgs/matching/admin.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from models import RGS_User, Interest
 
 # Register your models here.
+@admin.register(RGS_User)
+class RGS_UserAdmin(admin.ModelAdmin):
+	fields = ('name', 'lastname', 'gender', 'age', 'interests')
+
+@admin.register(Interest)
+class InterestAdmin(admin.ModelAdmin):
+	fields = ('desc',)
--- rgs/matching/models.py
+++ rgs/matching/models.py
@@ -2,5 +2,31 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
+class RGS_User(models.Model):
+	GENDER = (
+		('M', 'Male'),
+		('F', 'Female')
+	)
+	name = models.CharField(max_length=30)
+	lastname = models.CharField(max_length=100)
+	gender = models.CharField(max_length=1, choices=GENDER)
+	age = models.PositiveIntegerField(validators=[MinValueValidator(18)])
+	interests = models.ManyToManyField('Interest')
+
+	def __str__(self):
+		return str(self.name) + " " + str(self.lastname)
+
+	class Meta:
+		verbose_name = "RGS User"
+
+class Interest(models.Model):
+	desc = models.CharField(max_length=20)
+
+	def __str__(self):
+		return str(self.desc)
+
+	class Meta:
+		verbose_name = "Interest"
